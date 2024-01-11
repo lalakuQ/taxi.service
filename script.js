47e5c7435e43f7776c14cd4f1496e27d36c03c50
lalakuQ <vdyugay2004@gmail.com>
2024-01-11 22:10:00 +0500

    Fix: Burger menu doesn't reveal after resizing window

--- script.js
+++ script.js
@@ -1,66 +1,45 @@
+var clickHandlerActive = false;
 const mediaQuery = window.matchMedia('(max-width: 1001px)')
 const burger_menu = document.getElementById('burger')
 const nav = document.getElementById('nav');
 const sibling = nav.nextSibling;
-var burger_status = 0;
-function handle(res, obj){
-   var wrap = document.createElement('div');
-   wrap.style.display = 'none';
-   if (res.matches){
-      nav.parentNode.insertBefore(wrap, nav);
-      wrap.appendChild(nav);  
-      console.log(burger_status)
-         document.addEventListener('click', function(event){
-            console.log(burger_status)
-            if(obj.contains(event.target) && burger_status === 0){
-               wrap.style.display = 'flex';
-               wrap.style.width = '100%';
-               wrap.style.position = 'absolute'
-               wrap.style.top = '0';
-               wrap.style.left = '0';
-               wrap.style.height = '100vh';
-               nav.style.height = '100%';
-               nav.style.display = 'flex';
-               nav.style.justifyContent = 'flex-start';
-               nav.style.gridGap = '0';
-               nav.style.flexDirection = 'column';
-               nav.style.position = 'fixed';
-               nav.style.width = '320px';
-               nav.style.top = '0';
-               nav.style.left = '0';
-               nav.style.background = '#FFF';
-               nav.style.fontSize = '18px';
-               burger_status = 1;
-            }
-            else if ((burger_status === 1 && !nav.contains(event.target))) {
-               
-               nav.style.display = 'none';
-               wrap.style.display = 'none'
-               burger_status = 0;
-            } 
-               
-         })
-            
-      }
-      else if(!res.matches){
-         console.log('!res')
-         burger_menu.insertAdjacentElement('afterend', nav);
-         wrap.remove();
-         nav.style.display = 'grid'
-         nav.style.height = '';
-         nav.style.display = '';
-         nav.style.justifyContent = '';
-         nav.style.gridGap = '';
-         nav.style.flexDirection = '';
-         nav.style.position = '';
-         nav.style.width = '';
-         nav.style.top = '';
-         nav.style.left = '';
-         nav.style.background = '';
-         nav.style.fontSize = '';
-      }
-      
-   }
+var wrap;
+var burger_status = false;
+
+function clickHandler(event) {
+    if (clickHandlerActive && burger_menu.contains(event.target) && burger_status === false) {
+        wrap.className = 'burger-wrap-revealed';
+        nav.className = 'burger-nav-revealed';
+        burger_status = true;
+    } else if (clickHandlerActive && burger_status === true && !nav.contains(event.target)) {
+        nav.className = 'burger-menu-hidden';
+        wrap.className = 'burger-menu-hidden';
+        burger_status = false;
+    }
+}
+
+document.addEventListener('click', clickHandler);
+
+function handle(res, obj) {
+    if (res.matches) {
+        if (!wrap) {
+            wrap = document.createElement('div');
+            nav.parentNode.insertBefore(wrap, nav);
+            wrap.appendChild(nav);
+        }
+        wrap.className = 'burger-menu-hidden'
+        nav.className = 'burger-menu-hidden'
+        clickHandlerActive = true;
+    } else if (!res.matches) {
+        if (wrap) {
+            burger_menu.insertAdjacentElement('afterend', nav);
+            wrap.remove();
+            wrap = null;
+            burger_status = false;
+        }
+        nav.className = 'nav';
+        clickHandlerActive = false;
+    }
+}
 
 mediaQuery.addEventListener('change', function() { handle(this, burger_menu); });
-
