f1be72cd94113b2b1fc8042da9001020e9990116
lalakuQ <vdyugay2004@gmail.com>
2024-01-28 00:50:17 +0500

    Config login process

--- core/views.py
+++ core/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from models import CustomUser
+from forms import CustomUserCreationForm
 
-# Create your views here.
 
-def index(request):
+def homepage(request):
     return render(request, 'core/index.html')
+
+
+def register(request):
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+    context = {'login_form': form}
+    return render(request, 'core/login.html', context=context)
+
+
+def login(request):
+
+    return render(request, 'core/login.html')
