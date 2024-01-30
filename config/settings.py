a09d0654fc82dd9ceeebefd374d8455a3ed9178d
lalakuQ <vdyugay2004@gmail.com>
2024-01-30 23:05:52 +0500

    Fix style.css links

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 AUTH_USER_MODEL = 'core.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
