2360b8467a8ebff75eb2ea191ee46e3f2507aa41
lalakuQ <vdyugay2004@gmail.com>
2024-02-02 14:55:07 +0500

    Merge js files

---
+++ core/static/core/js/script.js
@@ -0,0 +1,100 @@
+let clickHandlerActive = false;
+let wrap;
+let burger_status = false;
+let profile_status = false;
+const mediaQuery = window.matchMedia('(max-width: 1001px)');
+const burger_menu = document.getElementById('burger');
+const nav = document.getElementById('nav');
+const profile = document.getElementById('profile');
+const user_pop = document.getElementById('user-pop');
+const auth_phone = document.getElementById('auth-phone');
+const auth_email = document.getElementById('auth-email');
+
+window.onload = function() {
+    var buttons = document.getElementsByTagName('button');
+    if (buttons){
+        for (var i = 0; i < buttons.length; i++){
+            buttons[i].addEventListener('click', function(event){
+                event.preventDefault();
+            });
+        }
+    }
+}
+
+function profile_clickHandler(event){
+    if (profile){
+        if (profile_status === false && profile.contains(event.target)){  
+            user_pop.classList.toggle('user-pop-hidden');
+            profile_status = true;
+        }
+        else if (profile_status === true && !user_pop.contains(event.target)){
+            user_pop.classList.toggle('user-pop-hidden');
+            profile_status = false;
+        }
+    }
+}
+    
+
+function auth_btn_clickHandler(event){
+    if(auth_phone){
+        if (auth_phone.contains(event.target) || auth_email.contains(event.target)){
+            auth_phone.classList.toggle('auth-option-active');
+            auth_email.classList.toggle('auth-option-active');
+        }
+    }
+    
+}
+
+function clickHandler(event) {
+    if(burger_menu){
+        if (clickHandlerActive && burger_menu.contains(event.target) && burger_status === false) {
+            wrap.className = 'burger-wrap-revealed';
+            nav.classList.toggle('burger-nav-revealed');
+            nav.classList.toggle('burger-nav-hidden');
+            burger_status = true;
+        } 
+        else if (clickHandlerActive && burger_status === true && !nav.contains(event.target)) {
+            nav.classList.toggle('burger-nav-revealed');
+            nav.classList.toggle('burger-nav-hidden');
+            wrap.className = 'burger-wrap-hidden';
+            burger_status = false;
+        }
+    }
+    
+}
+
+
+function handle(res) {
+    if(nav){
+
+        if (res.matches) {
+            if (!wrap) {
+                wrap = document.createElement('div');
+                nav.parentNode.insertBefore(wrap, nav);
+                wrap.appendChild(nav);
+            }
+            wrap.className = 'burger-wrap-hidden';
+            nav.className = 'burger-nav-hidden';
+            nav.removeAttribute('id');
+            clickHandlerActive = true;
+        } 
+        else if (!res.matches) {
+            if (wrap) {
+                burger_menu.insertAdjacentElement('afterend', nav);
+                wrap.remove();
+                wrap = null;
+                burger_status = false;
+            }
+            nav.removeAttribute('class');
+            nav.setAttribute('id', 'nav');
+            clickHandlerActive = false;
+        }
+    }
+}
+
+
+document.addEventListener('click', auth_btn_clickHandler)
+document.addEventListener('click', clickHandler);
+document.addEventListener('click', profile_clickHandler);
+handle(mediaQuery)
+mediaQuery.addEventListener('change', function() { handle(this, burger_menu); });
