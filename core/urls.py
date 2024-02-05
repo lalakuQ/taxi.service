830bf1d816b23561a3e3d5bb3aafd1e0d48e6c6e
lalakuQ <vdyugay2004@gmail.com>
2024-02-05 19:08:24 +0500

    Fix: logo redirect

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name=''),
-
+    path('', views.homepage, name='home'),
     path('login', views.login, name='login')
 ]
